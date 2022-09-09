3cc326d4cf754df440713d29a374c469e419579f
MKeru <michaelcwadih@gmail.com>
2022-09-09 12:06:55 -0500

    added slash command examples

--- index.js
+++ index.js
@@ -12,5 +12,21 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+	else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	}
+	else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
 // log in using token from .env
 client.login(process.env.BOT_TOKEN);
