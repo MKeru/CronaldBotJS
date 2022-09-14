31e82a841867a9ce92ef5a17758fe904fa30f95b
MKeru <michaelcwadih@gmail.com>
2022-09-14 12:46:01 -0500

    organized and consolidated

--- commands/register_GLOBAL.js
+++ commands/register_GLOBAL.js
@@ -15,67 +15,72 @@ module.exports = {
 		// allow each user to remove themself (DELETE user and all their data pertaining to that server)
 		// allow server manager to unregister the server (DELETE all data pertaining to the server)
 
+		// check if server is already registered for CronToken usage AND has CronToken Admin role
+		// if so, end interaction
+		const checkGuild = await Guilds.findOne({ where: { guildId: interaction.guild.id } });
+		const roleExists = interaction.guild.roles.cache.find(x => x.name === 'CronToken Admin');
+		if (checkGuild && roleExists) {
+			interaction.reply({ content: 'This server is already registered for CronToken usage and has the CronToken Admin role.', ephemeral: true });
+			return;
+		}
+		// check if server is already registered for CronToken usage and does NOT have CronToken Admin role
+		else if (checkGuild && !roleExists) {
+			// create CronToken Admin role in guild
+			const rm = new RoleManager(interaction.guild);
+			await rm.create({
+				name: 'CronToken Admin',
+				color: Colors.Grey,
+			});
+			interaction.reply({ content: 'This server is already registered for CronToken usage, but does not have CronToken Admin role (was it deleted?).\n\nIt has been generated.', ephemeral: true });
+			return;
+		}
+
+		// start building accept message
 		const row = new ActionRowBuilder()
 			.addComponents(
 				new ButtonBuilder()
 					.setCustomId('accept')
 					.setLabel('Accept')
 					.setStyle(ButtonStyle.Primary),
+			)
+			.addComponents(
+				new ButtonBuilder()
+					.setCustomId('decline')
+					.setLabel('Decline')
+					.setStyle(ButtonStyle.Secondary),
 			);
 
 		// warning message
 		await interaction.reply({ content: 'This will register this server for CronToken usage. The server ID and registered users\' IDs will be stored in a database.' +
 			'\n\nYou may remove the server data entirely or individual users may remove their data at any time.\n\nPlease click the button below to register!', components: [row], ephemeral: true });
 
+		const filter = i => (i.customId === 'accept' || i.customId === 'decline') && i.user.id === interaction.user.id.toString();
+		const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
+
 		try {
-
-			const filter = i => i.customId === 'accept' && i.user.id === interaction.user.id.toString();
-
-			const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
-
 			collector.on('collect', async i => {
-				// create RoleManager object
-				const rm = new RoleManager(interaction.guild);
-
-				const thisGuild = await Guilds.findOne({ where: { guildId: interaction.guild.id } });
-
-				// check if guild is already in list of registered guilds
-				if (!thisGuild) {
+				if (i.customId === 'accept') {
+					// add guildId to database
 					await Guilds.create({ guildId: interaction.guild.id });
-
-					// check if server does not have the crontoken admin role
-					if (!interaction.guild.roles.cache.find(x => x.name === 'CronToken Admin')) {
-
-						// generate a role that allows usage of admin-only commands for this bot
+					// check if CronToken Admin role does not exist
+					if (!roleExists) {
+						// create CronToken Admin role in guild
+						const rm = new RoleManager(interaction.guild);
 						await rm.create({
 							name: 'CronToken Admin',
 							color: Colors.Grey,
 						});
-
 						await i.update({ content: 'Server successfully registered for CronToken usage.\n\nPlease assign the "@CronToken Admin" role to users to entrust them with top-level CronToken management.', components: [] });
-						console.log(`CronToken role created in server ${interaction.guild.id}`);
+						console.log(`Server ${interaction.guild.id} was registered for CronToken usage and CronToken Admin role was generated successfully.`);
 					}
-					else {
-						await i.update({ content: 'Server successfully registered for CronToken usage.\n\nIt looks like the "@CronToken Admin" role is already in this server.\n\nAssign this role to users to entrust them with top-level CronToken management.', components: [] });
-						console.log(`CronToken role already in server ${interaction.guild.id}`);
+					else if (roleExists) {
+						await i.update({ content: 'Server successfully registered for CronToken usage.\n\nThe CronToken Admin role already exists in this server, so it was not generated.', components: [] });
+						console.log(`Server ${interaction.guild.id} was registered for CronToken usage. CronToken Admin role already exists.`);
 					}
-
-					console.log(`${interaction.guild.id} has been registered.`);
 				}
-				// check for crontoken role existence
-				else if (thisGuild && !interaction.guild.roles.cache.find(x => x.name === 'CronToken Admin')) {
-
-					await rm.create({
-						name: 'CronToken Admin',
-						color: Colors.Grey,
-					});
-
-					await i.update({ content: 'Server already registered for CronToken usage, but "@CronToken Admin" role was not found (was it deleted?).\n\nThe role has been generated. Please assign the "@CronToken Admin" role to users to entrust them with top-level CronToken management.', components: [] });
-					console.log(`CronToken role created in server ${interaction.guild.id}`);
-				}
-				else {
-					await i.update({ content: 'Server is already registered for CronToken usage.', components: [] });
-					console.log(`${interaction.guild.id} already registered for CronToken usage`);
+				else if (i.customId === 'decline') {
+					await i.update({ content: 'Declined registration for CronToken usage.', components: [] });
+					console.log(`Server ${interaction.guild.id} declined registration for CronToken usage.`);
 				}
 			});
 		}
