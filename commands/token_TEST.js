176fa230422f1deeb9a508c84a3da380168fbf14
MKeru <michaelcwadih@gmail.com>
2022-09-14 14:36:40 -0500

    no DM and changed wording

--- commands/token_TEST.js
+++ commands/token_TEST.js
@@ -4,9 +4,10 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('token')
 		.setDescription('CronToken command.')
+		.setDMPermission(false)
 		.addSubcommand(subcommand =>
 			subcommand
-				.setName('give')
+				.setName('generate')
 				.setDescription('Generate CronTokens for a user.')
 			// this command can only be used by the highest level cronaldbot role
 			// FIXME: set permission for that role
@@ -21,6 +22,6 @@ module.exports = {
 						.setDescription('The number of tokens to give the target user.')
 						.setRequired(true))),
 	async execute(interaction) {
-		await interaction.reply(`Gave ${interaction.options.getUser('user')} ${interaction.options.getInteger('number')} CronTokens.`);
+		await interaction.reply(`Generated ${interaction.options.getInteger('number')} CronTokens for ${interaction.options.getUser('user')}.`);
 	},
 };
